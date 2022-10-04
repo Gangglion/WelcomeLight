8660ff9e094990bab644413e4992a9eb6663f100
Gangglion <o0tmdguq0o@gmail.com>
2022-10-04 16:40:51 +0900

    WelcomeLightApp 틀 작성

---
+++ WelcomeLightApp/app/src/main/java/com/arduinoproject/welcomelight/view/FindDeviceActivity.kt
@@ -0,0 +1,13 @@
+package com.arduinoproject.welcomelight.view
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.arduinoproject.welcomelight.databinding.ActivityFindDeviceBinding
+import com.arduinoproject.welcomelight.viewmodel.ViewModel
+
+class FindDeviceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
---
+++ WelcomeLightApp/app/src/main/java/com/arduinoproject/welcomelight/viewmodel/ViewModel.kt
@@ -0,0 +1,7 @@
+package com.arduinoproject.welcomelight.viewmodel
+
+import androidx.databinding.BaseObservable
+
+class ViewModel : BaseObservable() {
+
+}
