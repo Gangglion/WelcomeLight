d4374ee086a818cae1d0f7cabbc92dc547ca352c
SeungHyeob Han <o0tmdguq0o@gmail.com>
2022-10-11 08:40:45 +0900

    edit - 수정 시도
    변경기능은 없으나 코드는 수정됨

--- WelcomeLightApp/app/src/main/java/com/glion/welcomelightapp/view/RegistdeviceActivity.kt
+++ WelcomeLightApp/app/src/main/java/com/glion/welcomelightapp/view/RegistdeviceActivity.kt
@@ -1,22 +1,35 @@
 package com.glion.welcomelightapp.view
 
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.glion.welcomelightapp.R
 import com.glion.welcomelightapp.databinding.ActivityRegistdeviceBinding
 import com.glion.welcomelightapp.viewmodel.BluetoothViewModel
+import java.util.jar.Manifest
 
 class RegistdeviceActivity : AppCompatActivity() {
     private lateinit var binding : ActivityRegistdeviceBinding
-    private var btViewModel = BluetoothViewModel()
+    private lateinit var btViewModel : BluetoothViewModel
+    lateinit var bluetoothManager: BluetoothManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_registdevice)
-        binding.viewmodel = btViewModel
-
+        bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        btViewModel = BluetoothViewModel(bluetoothManager)
+        binding.viewmodel = this.btViewModel
+        enableDisableBT()
         checkBTObserver()
     }
     private fun checkBTObserver(){
@@ -25,8 +38,22 @@ class RegistdeviceActivity : AppCompatActivity() {
             if(btViewModel.supportBT.value == false)
             {
                 Toast.makeText(this,"블루투스를 지원하지 않는 기기입니다. 앱을 종료합니다.",Toast.LENGTH_LONG).show()
-                finish()
+                // finish()
             }
         }
+    }
+    // 블루투스 사용할수 있는지 없는지 여부 체크 함수
+    private fun enableDisableBT(){
+        when
+        {
+            ContextCompat.checkSelfPermission(this,android.Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED->{
+                Log.d("tmdguq","1번호출 - checkSelfPermission")
+            }
+            shouldShowRequestPermissionRationale(android.Manifest.permission.BLUETOOTH_CONNECT)->{
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.BLUETOOTH_CONNECT),101)
+                Log.d("tmdguq","2번호출 - requestPermissions")
+            }
+        }
+
     }
 }
--- WelcomeLightApp/app/src/main/java/com/glion/welcomelightapp/viewmodel/BluetoothViewModel.kt
+++ WelcomeLightApp/app/src/main/java/com/glion/welcomelightapp/viewmodel/BluetoothViewModel.kt
@@ -1,31 +1,32 @@
 package com.glion.welcomelightapp.viewmodel
 
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.glion.welcomelightapp.model.Model
 
-class BluetoothViewModel {
+class BluetoothViewModel(bluetoothManager: BluetoothManager) {
     private var model : Model = Model()
     private val _supportBT = MutableLiveData<Boolean>()
     var supportBT : LiveData<Boolean> = _supportBT
-    private val mBluetoothAdapter : BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+    var bluetoothAdapter: BluetoothAdapter?
 
     init{
-        if(mBluetoothAdapter == null)
+        bluetoothAdapter = bluetoothManager.adapter
+        if(bluetoothAdapter == null)
             _supportBT.value = false
         else
         {
             _supportBT.value = true
-            Log.d("tmdguq","블루투스 목록 불러오기")
+            Log.d("tmdguq","블루투스 목록 불러오는 함수 호출")
         }
     }
     // 리스트 새로고침 함수
     fun refreshBtnClick(){
         Log.d("tmdguq","리스트 새로고침 - 블루투스 검색 함수 호출")
     }
-
     fun bTList(){
         // TODO :블루투스 목록 불러오는 함수(목록 불러오기 -> 리스트어탭터에 추가 -> 리스트뷰에 표시 순)
 
