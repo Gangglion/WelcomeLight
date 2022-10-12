d85855bd4a70858d0253f19fdecdd16cab03f651
K406-04\USER <bcucs20@gmail.com>
2022-10-12 19:41:04 +0900

    commit : 아두이노 코드 추가

---
+++ WelcomeLightDevice/build/sketch/graphicLCDPractice.ino.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#line 1 "d:\\welcomeLight\\graphicLCDPractice.ino"
+#include <Adafruit_GFX.h>
+#include <Adafruit_PCD8544.h>
+
+void setup()
+{
+	
+}
+
+void loop()
+{
+	
+}
+
---
+++ WelcomeLightDevice/graphicLCDPractice.ino
@@ -0,0 +1,12 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_PCD8544.h>
+
+void setup()
+{
+	
+}
+
+void loop()
+{
+	
+}
