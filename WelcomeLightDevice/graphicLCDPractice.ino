bfc454ee666f8197f17ab910422337d0dde6a092
Gangglion <o0tmdguq0o@gmail.com>
2022-10-19 21:56:09 +0900

    블루투스 예시코드 작성 - 테스트해보기

--- WelcomeLightDevice/graphicLCDPractice.ino
+++ WelcomeLightDevice/graphicLCDPractice.ino
@@ -1,12 +1,24 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_PCD8544.h>
 
+/*  아두이노 HC-06 예시코드 - Arduino bluetooth controller 깔아서 테스트하기 */
+#include <SoftwareSerial.h>
+
+#define BT_RXD 2
+#define BT_TXD 3
+SoftwareSerial hc06(BT_RXD,BT_TXD)
+
 void setup()
 {
-	
+	Serial.begin(9600);
+    hc06.begin(9600);
 }
 
 void loop()
 {
-	
+	if(hc06.available())
+        Serial.Write(hc06.read());
+    if(Serial.available())
+        hc06.write(Serial.read());
 }
+/*  아두이노 HC-06 예시코드 */
